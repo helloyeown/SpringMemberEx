2e495bcbffa102fec35f07f2b089cf868c807941
hiYewonMoon1 <toquasimodox@gmail.com>
2024-01-31 23:29:51 +0900

    SingletonTest

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,6 +1,7 @@
 package hello.core.xml;
 
 import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -9,12 +10,8 @@ public class XmlAppContext {
 
     @Test
     void xmlAppContext() {
-        ApplicationContext ac = new
-                GenericXmlApplicationContext("appConfig.xml");
-
-        MemberService memberService = ac.getBean("memberService",
-                MemberService.class);
-        assertThat(memberService).isInstanceOf(MemberService.class);
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
     }
-}
 }
